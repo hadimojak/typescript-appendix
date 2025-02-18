a8da47c0714af0d623e5d1227887f28388e93423
hadi arbabi <hadimojak@gmail.com>
2025-02-18 10:23:46 +0330

    types

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = __importDefault(require("axios"));
-const data = (0, axios_1.default)("https://jsonplaceholder.typicode.com/todos/1", { method: "GET" }).then((data) => console.log(data.data));
+(0, axios_1.default)("https://jsonplaceholder.typicode.com/todos/1", { method: "GET" }).then((response) => {
+    //   const { id, title, completed: finished }: Todo | Todo1 = response.data;
+    const { id, title, completed: finished } = response.data;
+    console.log(logTodo(id, title, finished));
+});
+const logTodo = (id, title, completed) => {
+    return `finished: ${completed}    title: ${title}   id : ${id}`;
+};
--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+type Todo = {
+  id: number;
+  title: string;
+  completed: boolean;
+};
+interface Todo1 {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
-const data = axios("https://jsonplaceholder.typicode.com/todos/1", { method: "GET" }).then((data) => console.log(data.data));
+axios("https://jsonplaceholder.typicode.com/todos/1", { method: "GET" }).then(
+  (response: AxiosResponse) => {
+    //   const { id, title, completed: finished }: Todo | Todo1 = response.data;
+    const { id, title, completed: finished } = <Todo>response.data;
+    console.log(logTodo(id, title, finished));
+  }
+);
+
+const logTodo = (id: number, title: string, completed: boolean): string => {
+  return `finished: ${completed}    title: ${title}   id : ${id}`;
+};
