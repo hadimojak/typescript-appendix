9f1dace65a5fbf5d9641381e91e0fe62cf6aed3f
hadi arbabi <hadimojak@gmail.com>
2025-04-09 12:19:00 +0330

    google map api

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,26 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+// import axios, { AxiosResponse } from "axios";
+// type Todo = {
+//   id: number;
+//   title: string;
+//   price: number;
+// };
 Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
-(0, axios_1.default)("https://jsonplaceholder.typicode.com/todos/1", { method: "GET" }).then((response) => {
-    //   const { id, title, completed: finished }: Todo | Todo1 = response.data;
-    const { id, title, completed: finished } = response.data;
-    console.log(logTodo(id, title, finished));
-});
-const logTodo = (id, title, completed) => {
-    return `finished: ${completed}    title: ${title}   id : ${id}`;
-};
+// const logTodo = (id: number, title: string, price: number): void => {
+//   console.log(`price: ${price}    title: ${title}   id : ${id}`);
+// };
+// const asyncFn = async () => {
+//   const response: AxiosResponse = await axios("https://dummyjson.com/products", { method: "GET" });
+//   const products: Todo[] = response.data.products;
+//   products.forEach((val) => logTodo(val.id, val.title, val.price));
+// };
+// try {
+//   asyncFn();
+// } catch (error) {
+//   console.log(error);
+// }
+const User_1 = require("./User");
+const Company_1 = require("./Company");
+const user = new User_1.User();
+const company = new Company_1.Company();
+console.log(user, company);
---
+++ src/index.ts
@@ -0,0 +1,30 @@
+// import axios, { AxiosResponse } from "axios";
+// type Todo = {
+//   id: number;
+//   title: string;
+//   price: number;
+// };
+
+// const logTodo = (id: number, title: string, price: number): void => {
+//   console.log(`price: ${price}    title: ${title}   id : ${id}`);
+// };
+
+// const asyncFn = async () => {
+//   const response: AxiosResponse = await axios("https://dummyjson.com/products", { method: "GET" });
+//   const products: Todo[] = response.data.products;
+//   products.forEach((val) => logTodo(val.id, val.title, val.price));
+// };
+
+// try {
+//   asyncFn();
+// } catch (error) {
+//   console.log(error);
+// }
+
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+const company = new Company();
+
+console.log(user, company);
